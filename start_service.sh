1a517b8a4ebe96bb8060040179c569e42b8970d8
Taylor Southwick <twsouthwick@outlook.com>
2024-11-03 18:55:25 -0800

    add user

--- start_service.sh
+++ start_service.sh
@@ -8,7 +8,7 @@ echo "Initializing db - see $LOGFILE for logs"
 perl /openils/bin/eg_db_config --update-config \
        --service all --create-database --create-schema --create-offline \
        --user $POSTGRES_USER --password $POSTGRES_PASSWORD --hostname $POSTGRES_HOST --port $POSTGRES_PORT \
-       --database $POSTGRES_DB > $LOGFILE
+       --database $POSTGRES_DB --admin-user $POSTGRES_USER --admin-pass $POSTGRES_PASSWORD > $LOGFILE
 echo "Finished initializing db"
 
 scanpids(){
