96a59ef7b407aa72774b5cb4748e4456a5819fed
Taylor Southwick <twsouthwick@outlook.com>
2024-11-06 16:35:28 -0800

    write db init to output stream

--- build/migrate-db.sh
+++ build/migrate-db.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-LOGFILE="/init/create_db.log"
-
-echo "Initializing db - see $LOGFILE for logs"
 perl /openils/bin/eg_db_config --create-database --create-schema \
        --user $POSTGRES_USER --password $POSTGRES_PASSWORD --hostname $POSTGRES_HOST --port $POSTGRES_PORT \
-       --database $POSTGRES_DB --admin-user $POSTGRES_USER --admin-pass $POSTGRES_PASSWORD > $LOGFILE
+       --database $POSTGRES_DB --admin-user $POSTGRES_USER --admin-pass $POSTGRES_PASSWORD
 echo "Finished initializing db"
