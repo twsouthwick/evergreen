4115c3b3c7eeabda8223ae5126c1ab5acca19617
Taylor Southwick <twsouthwick@outlook.com>
2024-11-06 23:21:55 -0800

    undo wss

--- build/start_httpd.sh
+++ build/start_httpd.sh
@@ -5,11 +5,11 @@ set -eux
 su -m opensrf -c /init-db.sh
 su - opensrf -c /openils/bin/autogen.sh
 
-: "${EVERGREEN_NO_SSL:=}"
+# : "${EVERGREEN_NO_SSL:=}"
 
-# Removing SSL for websockets
-if [ $EVERGREEN_NO_SSL ]; then
-  sed -i -e "s|'wss://'|'ws://'|g" /openils/var/web/js/dojo/opensrf/opensrf_ws.js
-fi
+# # Removing SSL for websockets
+# if [ $EVERGREEN_NO_SSL ]; then
+#   sed -i -e "s|'wss://'|'ws://'|g" /openils/var/web/js/dojo/opensrf/opensrf_ws.js
+# fi
 
 apachectl -D "FOREGROUND" -k start
