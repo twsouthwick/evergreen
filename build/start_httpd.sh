d8001d109f6bd7aded59ba74486201d27435e12c
Taylor Southwick <twsouthwick@outlook.com>
2024-11-06 16:35:39 -0800

    use mod_remoteip

--- build/start_httpd.sh
+++ build/start_httpd.sh
@@ -5,6 +5,8 @@ set -eux
 su -m opensrf -c /init-db.sh
 su - opensrf -c /openils/bin/autogen.sh
 
+: "${EVERGREEN_NO_SSL:=}"
+
 # Removing SSL for websockets
 if [ $EVERGREEN_NO_SSL ]; then
   sed -i -e "s|'wss://'|'ws://'|g" /openils/var/web/js/dojo/opensrf/opensrf_ws.js
